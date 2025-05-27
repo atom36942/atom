21a76028de17142cab4cc3044c458a1e041e6aaf
atom36942123 <atom36942@gmail.com>
2025-05-27 13:33:23 +0530

    sync

--- api.py
+++ api.py
@@ -18,6 +18,7 @@ from fastapi_limiter.depends import RateLimiter
 @router.get("/test",dependencies=[Depends(RateLimiter(times=1,seconds=1))])
 async def test(request:Request):
    object={"title":"test"}
-   await postgres_create("test",[object],0,request.state.postgres_client,postgres_column_datatype,object_serialize)
+   table="test"
+   await postgres_create(table,[object],0,request.state.postgres_client,postgres_column_datatype,object_serialize)
    return {"status":1,"message":"welcome to test"}
 
