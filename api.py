80b51a3b35bc9c6d9e5dc071251e5792f7484ede
atom36942123 <atom36942@gmail.com>
2025-06-02 23:08:05 +0530

    sync

--- api.py
+++ api.py
@@ -2,23 +2,23 @@
 from fastapi import APIRouter
 router=APIRouter()
 
-#import
-from main import *
-
 #env
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
+#function
+from function import *
 
 #ratelimiter
 from fastapi import Depends
 from fastapi_limiter.depends import RateLimiter
 
 #test
-@router.get("/test",dependencies=[Depends(RateLimiter(times=1,seconds=1))])
+@router.get("/api",dependencies=[Depends(RateLimiter(times=1,seconds=1))])
 async def test(request:Request):
    table="test"
-   object={"title":"test"}
+   object={"title":"test_api"}
    await postgres_create(table,[object],1,request.state.postgres_client,request.state.postgres_column_datatype,object_serialize)
    return {"status":1,"message":"welcome to test"}
 
