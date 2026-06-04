4957c170057639bb40aeed01c373fc7a2bd43f75
atom <atom36942@gmail.com>
2026-06-04 15:55:16 +0530

    sync

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ async def func_lifespan(app:"FastAPI"):
         client_redis = redis.Redis.from_pool(redis.ConnectionPool.from_url(app.state.config_redis_url)) if app.state.config_redis_url else None
         client_redis_producer = redis.Redis.from_pool(redis.ConnectionPool.from_url(app.state.config_redis_queue_url)) if app.state.config_redis_queue_url else None
         client_mongodb = motor.motor_asyncio.AsyncIOMotorClient(app.state.config_mongodb_url) if app.state.config_mongodb_url else None
-        client_mssql = await aioodbc.create_pool(dsn=app.state.config_mssql_url) if app.state.config_mssql_url else None
+        client_mssql = await aioodbc.create_pool(dsn=app.state.config_mssql_url, pool_recycle=900) if app.state.config_mssql_url else None
         client_s3 = aiobotocore.session.get_session().create_client("s3", region_name=app.state.config_s3_region_name, aws_access_key_id=app.state.config_aws_access_key_id, aws_secret_access_key=app.state.config_aws_secret_access_key) if app.state.config_s3_region_name else None
         client_s3_resource = boto3.resource("s3", region_name=app.state.config_s3_region_name, aws_access_key_id=app.state.config_aws_access_key_id, aws_secret_access_key=app.state.config_aws_secret_access_key) if app.state.config_s3_region_name else None
         client_sns = boto3.client("sns", region_name=app.state.config_sns_region_name, aws_access_key_id=app.state.config_aws_access_key_id, aws_secret_access_key=app.state.config_aws_secret_access_key) if app.state.config_sns_region_name else None
